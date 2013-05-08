09dd8c1545577aa153382b3fff41fcd8acc3862e
Milosz Kosmider <milosz@milosz.ca>
2013-05-08 01:14:52 -0400

    Now with suggestion lists!

--- atomicity.js
+++ atomicity.js
@@ -30,8 +30,11 @@ $(document).ready(function() {
 	});
 
 	$('#input .add.entry .add').on('click', function() {
-		addEntry();
+		var $entry = addEntry();
 		populateXmlFromFields();
+		if ($('#input .expanded.entry').length === 0) {
+			$entry.find('.expand').trigger('click');
+		}
 	});
 
 	$('#input').on('click', '.entry .expand', function() {
@@ -88,6 +91,7 @@ $(document).ready(function() {
 		$addEntry.before($entry);
 
 		$entry.find('.default').trigger('click');
+		wireInput($entry.find('.input'));
 
 		return $entry;
 	}
@@ -116,25 +120,32 @@ $(document).ready(function() {
 					$input = $section.find('.input'),
 					fieldValue = $input.val();
 
+				if ($section.hasClass('timestamp')) {
+					var currentTime = moment(fieldValue);
+					fieldValue = currentTime !== null && currentTime.isValid()
+						? currentTime.utc().format('YYYY-MM-DDThh:mm:ss\\Z')
+						: '';
+				}
+
 				$.each(fieldNames, function(_, fieldName) {
-					if ($section.hasClass('timestamp')) {
-						var currentTime = moment(fieldValue);
-						fieldValue = currentTime !== null && currentTime.isValid()
-							? currentTime.utc().format('YYYY-MM-DDThh:mm:ss\\Z')
-							: '';
+					if ($section.hasClass('multi')) {
+						var attr = $section.data('attr');
+						var $nodes = $($.map(fieldValue.split(/\s*,\s*/), function(value) {
+							return value.length > 0 ? $('<' + fieldName + '/>').attr(attr, value).get(0) : null;
+						}));
+					}
+					else {
+						var $nodes = $('<' + fieldName + '>').text(fieldValue);
 					}
 
-					var $node = $('<' + fieldName + '>');
-					$node.text(fieldValue);
-
-					$container.append($node);
+					$container.append($nodes);
 				});
 			});
 		});
 
 		var xmlBodyText = new XMLSerializer().serializeToString($root.get(0));
 		var xmlHeaderText = '<?xml version="1.0" encoding="' + document.characterSet + '" ?>'
-		$('#output .view').removeClass('prettyprinted').text(vkbeautify.xml(xmlHeaderText + xmlBodyText));
+		$('#output .view').removeClass('prettyprinted').text(selfCloseTags(vkbeautify.xml(xmlHeaderText + xmlBodyText)));
 
 		prettyPrint();
 	}
@@ -143,6 +154,8 @@ $(document).ready(function() {
 		var xmlParser = new DOMParser();
 		var xmlDoc = xmlParser.parseFromString(xml, 'text/xml');
 		var $xmlDoc = $(xmlDoc);
+
+		$('#input .section-group.entry:not(.add)').remove();
 		
 		(function recurse($nodes, $container) {
 			$.each($nodes, function(_, node) {
@@ -153,22 +166,117 @@ $(document).ready(function() {
 				}
 				else {
 					var fieldName = node.nodeName,
-						fieldValue = node.textContent,
-						$section = $container.find('.section[data-name="' + fieldName + '"]'),
+						$section = $container.find('.section[data-name~="' + fieldName + '"]'),
+						attribute = $section.data('attr'),
 						$input = $section.find('.input');
+
+					if (attribute !== undefined) {
+						var fieldValue = node.getAttribute(attribute);
+					}
+					else {
+						var fieldValue = node.textContent;
+					}
 
 					if ($section.hasClass('timestamp')) {
 						fieldValue = prettyTime(fieldValue);
 					}
 
-					$input.val(fieldValue);
+					if ($section.hasClass('multi')) {
+						$input.val(extendCommaSeparatedList($input.val(), fieldValue));
+					}
+					else {
+						$input.val(fieldValue);
+					}
 				}
 			});
 		})($xmlDoc.find('feed > *'), $('#input .feed.section-group'));
 	}
 
+	function extendCommaSeparatedList(list, value) {
+		return /^\s*$/.test(list) ? value : (list.replace(/(?:\s*,\s*)?$/, '') + ', ' + value);
+	}
+
 	function prettyTime(time) {
 		return moment(time).format('MMMM D, YYYY, HH:mm:ss');
+	}
+
+	// Since autocomplete can't be bound as a live handler
+	function wireInput($inputs) {
+		$inputs.each(function(_, input) {
+			var $input = $(input),
+				$section = $input.parents('.section');
+
+			if ($section.hasClass('select')) {
+				// don't navigate away from the field on tab when selecting an item
+				$input.on('keydown', function(event) {
+					if (event.keyCode === $.ui.keyCode.TAB && $(this).data('ui-autocomplete').menu.active) {
+						event.preventDefault();
+					}
+				}); 
+
+				$input.on('blur', function() {
+					$input.val($input.val().replace(/\s*,\s*$/, ''));
+				});
+
+				$input.autocomplete({
+					delay: 0,
+					minLength: 0,
+					source: function(request, response) {
+						var choices = $section.data('choices');
+
+						if (choices instanceof Array) {
+							; // done
+						}
+						else if (typeof choices === 'string') {
+							var parts = choices.split(';'),
+								attribute = parts[0],
+								source = parts[1];
+
+							if (source === 'other') {
+								var $otherSections = $('#input .entry.section-group').not($section.parents('.section-group')).find('.section[data-name~="' + attribute + '"]');
+								choices = $.map($otherSections, function(otherSection) {
+									return $(otherSection).find('.input').val();
+								});
+							}
+						}
+
+						var eligibleTerms = request.term.split(/\s*,\s*/);
+						var lastTerm = eligibleTerms.pop();
+						var chosenTerms = $.ui.autocomplete.filter(subtractArray(choices, eligibleTerms), lastTerm);
+						response(chosenTerms);
+					},
+					focus: function() {
+						// prevent value inserted on focus
+						return false;
+					},
+					select: function(event, ui) {
+						var terms = $input.val().split(/\s*,\s*/);
+						// remove the current input
+						terms.pop();
+						// add the selected item
+						terms.push(ui.item.value);
+						// add placeholder to get the comma-and-space at the end
+						terms.push('');
+						$input.val(terms.join(', '));
+						return false;
+					}
+				});
+			}
+		});
+	}
+
+	function subtractArray(minuend, subtrahend) {
+		var hash = {};
+		$.each(subtrahend, function(_, item) {
+			hash[item.toLowerCase()] = 1;
+		});
+		return $.grep(minuend, function(item) {
+			return hash[item.toLowerCase()] === undefined;
+		});
+	}
+
+	function selfCloseTags(xmlString) {
+		return xmlString.replace(/><\/.+?>/g, ' />');
 	}
 
 });
